e88f3d3853aa7103fa3027a016fdd9f782e47fb6
JeroenvanDoorenmalen <j.v.doorenmalen@student.tue.nl>
2018-01-20 17:27:39 +0100

    Added total at bottom of data tables

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -425,8 +425,42 @@ function buildTable() {
 
 this.createTable = function() {
 	$(document).ready(function(){
-	$('#DataTable').DataTable({ paging: false, bLengthChange: false});
-	});
+	$('#DataTable').DataTable({ 
+	paging: false,
+	bLengthChange: false,
+	    "footerCallback": function ( row, data, start, end, display ) {
+            var api = this.api(), data;
+ 
+            // Remove the formatting to get integer data for summation
+            var intVal = function ( i ) {
+                return typeof i === 'string' ?
+                    i.replace(/[\$,]/g, '')*1 :
+                    typeof i === 'number' ?
+                        i : 0;
+            };
+ 
+            // Total over all pages
+            total = api
+                .column( 2 )
+                .data()
+                .reduce( function (a, b) {
+                    return intVal(a) + intVal(b);
+                }, 0 );
+ 
+            // Total over this page
+            pageTotal = api
+                .column( 2, { page: 'current'} )
+                .data()
+                .reduce( function (a, b) {
+                    return intVal(a) + intVal(b);
+                }, 0 );
+ 
+            // Update footer
+            $( api.column( 2 ).footer() ).html(
+                '$'+pageTotal +' (total +' total)'
+			);}
+		});
+	})
 }
 
 function createChart(){
