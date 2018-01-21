aca00d60b26e229fd25a479b84ddab4a9c0dba22
JeroenvanDoorenmalen <j.v.doorenmalen@student.tue.nl>
2018-01-21 15:09:14 +0100

    Changed # years with charts

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -474,7 +474,7 @@ function createChart(){
 	var myChart = new Chart(ctx, {
 	type: 'line',
 	data: {
-		labels: ["2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010"],
+		labels: ["2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010","2011","2012"],
 		datasets: []
 	},
 	options: {
@@ -499,7 +499,7 @@ function createChart1(){
 	var myChart = new Chart(ctx, {
 	type: 'bar',
 	data: {
-		labels: ["2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010"],
+		labels: ["2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010","2011","2012"],
 		datasets: []
 	},
 	options: {
@@ -523,7 +523,7 @@ function createChart2(){
 	var myChart = new Chart(ctx, {
 	type: 'line',
 	data: {
-		labels: ["2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010"],
+		labels: ["2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010","2011","2012"],
 		datasets: []
 	},
 	options: {
@@ -544,7 +544,7 @@ function createChart3(){
 	var myChart = new Chart(ctx, {
 	type: 'bar',
 	data: {
-		labels: ["2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010"],
+		labels: ["2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010","2011","2012"],
 		datasets: []
 	},
 	options: {
@@ -566,7 +566,7 @@ function createChart5(){
 	var myChart = new Chart(ctx, {
 	type: 'radar',
 	data: {
-		labels: ["2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010"],
+		labels: ["2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010","2011","2012"],
 		datasets: []
 	},
 	options: {
@@ -589,7 +589,7 @@ var enddata = [];
 for (j=0; j < selectedcountries.length; j++){
 	 var temp = data[selectedcountries[j]];
 		for(var l in temp){
-			if (l > 2000 && l < 2011){
+			if (l > 2000 && l < 2013){
 				if (!(enddata[j])) {
 					enddata[j]=[];
 				}
